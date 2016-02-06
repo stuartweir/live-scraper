3c32c9ea93bb77776126ee255816ac7b7ccc01cc
stuartweir <cameron.weir@mytwu.ca>
2016-02-06 15:15:11 -0800

    Clean up data.go file for readability

--- data.go
+++ data.go
@@ -1,10 +1,9 @@
-// 4. prepping data for user (making struct + JSON)
 package main
 
 type Amazon_ID struct {
-	Title       string   `json: "title"`        // h1 id="aiv-content-title"
-	ReleaseYear int      `json: "release_year"` // span class="releease-year"
-	Actors      []string `json: "actors"`       // dl class="dv-meta-info size-small" dd -> names, not nums
+	Title       string   `json: "title"`        // id="aiv-content-title"
+	ReleaseYear int      `json: "release_year"` // span class="release-year"
+	Actors      []string `json: "actors"`       // class="dv-meta-info size-small" dd -> names
 	Poster      string   `json: "posters"`      // div class="dp-meta-icon-container" -> img src
-	SimilarIDs  []string `json: "similar_ids"`  // ul class="shelf cf" -> li data-asin=ID
+	SimilarIDs  []string `json: "similar_ids"`  // ul class="shelf" -> li data-asin=ID
 }
